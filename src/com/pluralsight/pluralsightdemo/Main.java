b3e2f84088f67c8786bdc127d2bd1c6a0d2c29dd
nasirjamal <shaikh_nasirjamal@yahoo.com>
2018-10-03 00:19:42 -0400

    Revert "test commit"
    This reverts commit 6bd0c111f68a60cd27bf362f76b6908351377a81, reversing
    changes made to 90736549267b89ef585bad41efe96e8295658dc8.

--- src/com/pluralsight/pluralsightdemo/Main.java
+++ src/com/pluralsight/pluralsightdemo/Main.java
@@ -2,8 +2,7 @@ package com.pluralsight.pluralsightdemo;
 import java.io.BufferedReader;
 import java.util.*;
 public class Main {
-//Nasir.........
-    //test
+
     public static void main(String[] args) {
         while(true)
         {
