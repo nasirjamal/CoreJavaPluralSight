e4b47165203b0bf1ab0264b52f28e13b90610c0a
Nasirjamal Shaikh <shaikh_nasirjamal@yahoo.com>
2018-10-02 23:38:07 -0400

    Add files via upload

---
+++ out/production/PluralsightDemo/com/pluralsight/pluralsightdemo/Conversion.java
@@ -0,0 +1,21 @@
+package com.pluralsight.pluralsightdemo;
+
+public class Conversion {
+
+    public void conversion()
+    {
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("In conversion...");
+        float floatVal=1.04f;
+        double doubleVal=4.0d;
+        byte byteVal=7;
+        short shortVal=7;
+        long longVal=30;
+
+        short result1= (short)longVal;
+        long result2= (short) byteVal-longVal;
+
+        System.out.println("Converted---> Result 1: "+result1+" || Result2: "+result2);
+        System.out.println("-----------------------------------------------------------------------------");
+    }
+}
